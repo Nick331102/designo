fb953b5801d540fb4c324d63290ab2a2b0b01291
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-12-28 09:06:23 -0500

    Add files via upload

--- validate.js
+++ validate.js
@@ -1,14 +1,16 @@
-const form = document.querySelector(".contact__form");
-const input = document.getElementById("input")
-const errorMessage = document.querySelectorAll(".error__message");
+
 
 function errorMessages() {
+
+    const form = document.querySelector(".contact__form");
+    const userInput = document.querySelector("#input")
+    const errorMessage = document.querySelectorAll(".error__message");
+
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        if (input.value.length < 1 || input.value === null) {
+        if (userInput.value === "") {
             errorMessage.classList.add("show")
         }
     })
 }
-
 errorMessages()
