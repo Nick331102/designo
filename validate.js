f2600966b84c8cde0ad860df116a549b9a3c1e00
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-12-28 13:06:50 -0500

    Add files via upload
    Form validation error messages JS fixed

--- validate.js
+++ validate.js
@@ -1,16 +1,35 @@
+const form = document.querySelector(".contact__form");
+const userName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const errorMessage = document.querySelector(".error__message")
+const emailError = document.querySelector(".error__message--email")
+const phoneError = document.querySelector(".error__message--phone")
 
-
-function errorMessages() {
-
-    const form = document.querySelector(".contact__form");
-    const userInput = document.querySelector("#input")
-    const errorMessage = document.querySelectorAll(".error__message");
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        if (userInput.value === "") {
-            errorMessage.classList.add("show")
-        }
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  checkInputs();
     })
+
+function checkInputs() {
+    const userNameValue = userName.value.trim()
+    const emailValue = email.value.trim()
+    const phoneValue = phone.value.trim()
+
+    if (userNameValue === "") {
+        errorMessage.classList.add("show")
 }
-errorMessages()
+    if (emailValue === "") {
+        emailError.classList.add("show")
+}
+    if (phoneValue === "") {
+        phoneError.classList.add("show")
+        
+}
+}
+
+
+
+
+
+
